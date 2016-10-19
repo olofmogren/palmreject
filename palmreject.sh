6eacdecb8540f742cd6b16973db71409a073a077
Olof Mogren <olof@mogren.one>
2016-10-19 10:50:03 +0200

    GPL

--- palmreject.sh
+++ palmreject.sh
@@ -4,7 +4,20 @@
 touchscreen_device=12
 stylus_device=13
 
-# code. only change this if you know what you are doing.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# code follows. only change this if you know what you are doing.
 while [ 1 ]
 do
   state=`xinput query-state "$stylus_device" | grep -c "ValuatorClass Mode=Absolute Proximity=In"`
